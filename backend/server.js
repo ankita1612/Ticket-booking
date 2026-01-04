2e0c5043027f6c9fbc89423aa70afbdad68c457c
ANKITA <ankita.mca2007@gmail.com>
2026-01-04 16:20:36 +0530

    change server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
-
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import http from "http";
-import { Server } from "socket.io";
 import connectDB from "./src/config/db.js";
 import eventRoutes from "./src/routes/event.js";
 import { fileURLToPath } from "url";
 import path from "path";
+import { initSocket } from "./src/socket/index.js";
 
+process.on("uncaughtException", (err) => {
+  console.error(" UNCAUGHT EXCEPTION");
+  console.error(err.name, err.message);
+  process.exit(1);
+});
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
@@ -25,23 +27,7 @@ connectDB();
 app.use("/events", eventRoutes);
 
 const server = http.createServer(app);
-
-export const io = new Server(server, {
-  cors: {
-    origin: "*", // update in production
-    methods: ["GET", "POST"]
-  }
-});
-
-io.on("connection", (socket) => {
-  socket.on("join-event", (eventId) => {
-    socket.join(eventId);
-    console.log(`Socket ${socket.id} joined event ${eventId}`);
-  });
-
-  socket.on("disconnect", () => {
-  });
-});
+initSocket(server);
 
 // Serve frontend
 const __filename = fileURLToPath(import.meta.url);
@@ -54,6 +40,25 @@ app.use(express.static(frontendPath));
 app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
 });
+//End Serve Frontend
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION");
+  console.error(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received. Shutting down gracefully...");
+  server.close(() => {
+    process.exit(0);
+  });
 });
